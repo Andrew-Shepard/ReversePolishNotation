55bb0a7e8924fd446135c96238f5576c9eb915bc
andre <andrewshepard78@gmail.com>
2021-11-06 12:08:56 -0400

    Finshed infix to postfix conversion

--- src/infix.cpp
+++ src/infix.cpp
@@ -6,18 +6,65 @@
 #include <sstream>
 #include "Stack.h"
 #include "ReversePolishNotationCalculator.h"
+bool isAnOperator(std::string entry) { // determines if a string is an operator
+    char character = entry[0];
+    switch (character) {
+        case '+': return true;
+        case '-': return true;
+        case '/': return true;
+        case 'x': return true;
+        default: return false;
+    }
+}
+int precedence(char c){ //Determines the precedence of an operator
+    switch (c){
+        case '(': return 1;
+        case '+':
+        case '-': return 2;
+        case '*':
+        case '/': return 3;
+    }
+    return -1;
+}
 int main(int argc, char** argv)
 {
     ReversePolishNotationCalculator reversePolishNotationCalculator(1000);
     std::stringstream arguments;
     std::string s;
+    Stack<std::string> operations(1000);
     //ignore the path call and take the arguments
     for (int i = 1; i < argc; ++i){
         arguments << argv[i];
         s = arguments.str();
-        reversePolishNotationCalculator.push(s);
+        //if it is an operator
+        if(isAnOperator(s)){
+            //if the stack is empty push the to the operator stack
+            if (operations.getLength() == 0){
+                operations.push(s);
+            } else{
+                //compare the precedence of the operators
+                std::string top_operation = *operations.top();
+                if (precedence(s[0]) <= precedence(top_operation[0])){
+                    // if the new operator exceeds the precedence of the former top then add to the rpn
+                    reversePolishNotationCalculator.push(top_operation);
+                    operations.pop();
+                    operations.push(s);
+                } else {
+                    //otherwise add it to the stack
+                    operations.push(s);
+                }
+            }
+        } else {
+            //push operands to the calculator
+            reversePolishNotationCalculator.push(s);
+        }
         arguments.str("");
         arguments.clear();
+    }
+    while (operations.getLength() > 0){
+        //append the remaining operators to the rpnstack
+        reversePolishNotationCalculator.push(*operations.top());
+        operations.pop();
     }
 
     std::cout << reversePolishNotationCalculator.evaluate() << std::endl;
